20d9bde44631944f01f38ad0407a62192a5e4ff7
dominicod <dominicodonnell99@gmail.com>
2023-09-30 18:35:59 -0500

    PP-1 Move models to different folders

--- Identity.API/DTOs/UserDto.cs
+++ Identity.API/DTOs/UserDto.cs
@@ -1,16 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Identity.API.Models;
 using Utilities.Shared.DTOs;
-using Utilities.Shared.Models;
 
 namespace Identity.API.DTOs;
 
-public class UserDTO : BaseUserDTO
+public class UserDTO : BaseDTO
 {
+    [Required]
+    [StringLength(100)]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    
     public UserDTO()
     {
     }
 
-    public UserDTO(BaseUser user)
+    public UserDTO(User user)
     {
+        Id = user.Id;
         Email = user.Email;
         UserName = user.UserName;
         
--- Identity.API/Models/User.cs
+++ Identity.API/Models/User.cs
@@ -1,16 +1,25 @@
-using Utilities.Shared.DTOs;
+using System.ComponentModel.DataAnnotations;
+using Identity.API.DTOs;
 using Utilities.Shared.Models;
 
 namespace Identity.API.Models;
 
-public class User : BaseUser
+public class User : BaseModel
 {
+    [Required]
+    [StringLength(100)]
+    public string UserName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    
     public User()
     {
     }
 
-    public User(BaseUserDTO userDTO)
+    public User(UserDTO userDTO)
     {
+        Id = userDTO.Id;
         Email = userDTO.Email;
         UserName = userDTO.UserName;
     }
