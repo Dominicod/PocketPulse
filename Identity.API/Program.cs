cfe87ec402d8af944c699ddf7a2f659e02b8da16
dominicod <dominicodonnell99@gmail.com>
2023-09-30 21:14:35 -0500

    PP-1 Configure Serilog

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -1,33 +1,46 @@
 using Identity.API.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Web;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-
-builder.Services.AddDbContext<IIdentityDBContext, IdentityDBContext>(options =>
-    options.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"] ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found."))
-);
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
+
+    builder.Services.AddDbContext<IIdentityDBContext, IdentityDBContext>(options =>
+        options.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"] ??
+                             throw new InvalidOperationException("Connection string 'DefaultConnection' not found."))
+    );
+    // builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    //     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSerilog(loggerConfig =>
+    {
+        loggerConfig
+            .Enrich.FromLogContext()
+            .WriteTo.Async(wt => wt.Console())
+            .WriteTo.File("../logs/IdentityAPI.txt", rollingInterval: RollingInterval.Day);
+    });
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseSerilogRequestLogging();
+
+    // app.UseAuthentication();
+    // app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-// app.UseAuthentication();
-// app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex) { Log.Fatal("Error occured while starting Identity.API: Ex: {Ex}", ex.Message); }
+finally { Log.CloseAndFlush(); }
--- WebGateWay/Program.cs
+++ WebGateWay/Program.cs
@@ -1,19 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
-var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.MapControllers();
-app.UseEndpoints(endpoints =>
+try
 {
-    endpoints.MapReverseProxy();
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddReverseProxy()
+        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    builder.Services.AddSerilog(loggerConfig =>
+    {
+        loggerConfig
+            .Enrich.FromLogContext()
+            .WriteTo.Async(wt => wt.Console())
+            .WriteTo.File("../logs/WebGateWay.txt", rollingInterval: RollingInterval.Day);
+    });
+
+    var app = builder.Build();
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseSerilogRequestLogging();
+    app.MapControllers();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapReverseProxy();
+    });
+
+    app.Run();
+}
+catch (Exception ex) { Log.Fatal("Error occured while starting Identity.API: Ex: {Ex}", ex.Message); }
+finally { Log.CloseAndFlush(); }
