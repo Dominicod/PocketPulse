1225f7563235023a39235fc7642e03ba3b6c8b01
dominicod <dominicodonnell99@gmail.com>
2023-10-08 14:03:37 -0500

    PP-3 Create basic controller structure

---
+++ Financial.API/Controllers/BillController.cs
@@ -0,0 +1,45 @@
+using Financial.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Utilities.Shared;
+
+namespace Financial.API.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class BillController : BaseController<BillController>
+{
+    private readonly ILogger<BillController> _logger;
+
+    public BillController(ILogger<BillController> logger) : base(logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllBillsForUser(Guid userId)
+    {
+        _logger.LogInformation("Getting all bills for user {UserId}", userId);
+        return Ok("Hello World");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateBills(List<BillDTO> bills)
+    {
+        _logger.LogInformation("Creating bills");
+        return Ok("Hello World");
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateBills(List<BillDTO> bills)
+    {
+        _logger.LogInformation("Updating bills");
+        return Ok("Hello World");
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBills(List<Guid> billIds)
+    {
+        _logger.LogInformation("Deleting bills");
+        return Ok("Hello World");
+    }
+}
