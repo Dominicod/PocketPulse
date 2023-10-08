70fec226a2856a26d8e571b76a8523ef30653f37
dominicod <dominicodonnell99@gmail.com>
2023-10-08 10:15:36 -0500

    PP-2 Create BillType Enum

---
+++ Utilities/Enums/BillType.cs
@@ -0,0 +1,16 @@
+namespace Utilities.Enums;
+
+public enum BillType
+{
+    Housing = 0,
+    HousingInsurance = 1,
+    CarNote = 2,
+    CarInsurance = 3,
+    Phone = 4,
+    Internet = 5,
+    Electricity = 6,
+    Water = 7,
+    CreditCard = 8,
+    StudentLoan = 9,
+    Miscellaneous = 10
+}
