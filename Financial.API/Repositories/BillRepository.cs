4dc6bee03da5e5441f8325c5fb4c6dc4f4ffe731
dominicod <dominicodonnell99@gmail.com>
2023-10-19 22:58:10 -0500

    PP-4 Create Repository for Bills

---
+++ Financial.API/Repositories/BillRepository.cs
@@ -0,0 +1,64 @@
+using Financial.API.Data;
+using Financial.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financial.API.Repositories;
+
+public class BillRepository
+{
+    private readonly FinancialDBContext _context;
+
+    public BillRepository(FinancialDBContext context) => _context = context;
+
+    public async Task<Bill?> GetBill(Guid billId) => await _context.Bills.FindAsync(billId);
+
+    public async Task<List<Bill>> GetAllBills() => await _context.Bills.OrderBy(i => i.Id).ToListAsync();
+
+    public async Task<Bill> CreateBill(Bill bill)
+    {
+        await _context.Bills.AddAsync(bill);
+        await _context.SaveChangesAsync();
+
+        return bill;
+    }
+    
+    public async Task<List<Bill>> CreateBills(List<Bill> bills)
+    {
+        await _context.Bills.AddRangeAsync(bills);
+        await _context.SaveChangesAsync();
+
+        return bills;
+    }
+
+    public async Task<bool> DeleteBill(Bill bill)
+    {
+        _context.Remove(bill);
+        var result = await _context.SaveChangesAsync();
+
+        return result >= 1;
+    }
+    
+    public async Task<bool> DeleteBills(List<Bill> bills)
+    {
+        _context.RemoveRange(bills);
+        var result = await _context.SaveChangesAsync();
+
+        return result == bills.Count;
+    }
+
+    public async Task<Bill> UpdateBill(Bill bill)
+    {
+        _context.Bills.Update(bill);
+        await _context.SaveChangesAsync();
+            
+        return bill;
+    }
+
+    public async Task<List<Bill>> UpdateBills(List<Bill> bills)
+    {
+        _context.Bills.UpdateRange(bills);
+        await _context.SaveChangesAsync();
+            
+        return bills;
+    }
+}
